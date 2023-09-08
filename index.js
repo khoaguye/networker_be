1967b2d9e3a5a17fd313babed3e628a44cf83962
khoaguye <Knguyenn72@gmail.com>
2023-09-08 09:33:19 -0500

    fix cors

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ app.use(express.json());
 app.use("/people", peopleRoutes)
 
 
-const whitelist = ['http://localhost:3000']
+// const whitelist = ['http://localhost:3000']
 
-const corsOptions = {
-    origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error())
-      }
-    }
-  }
-  app.use(cors(corsOptions))
-  app.use(cors({
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
-}));
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//       if (whitelist.indexOf(origin) !== -1) {
+//         callback(null, true)
+//       } else {
+//         callback(new Error())
+//       }
+//     }
+//   }
+//   app.use(cors(corsOptions))
+//   app.use(cors({
+//     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+// }));
 
 app.listen(8000, () => {
     console.log("connected")
