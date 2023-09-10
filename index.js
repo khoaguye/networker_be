28204bb05437f9d853d41966773b241751a33cd0
khoaguye <Knguyenn72@gmail.com>
2023-09-10 12:50:25 -0500

    task schedule CronJob

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from "express"
 import peopleRoutes  from "./Route/people.js"
 import cors from "cors"
+import { CronJob } from "cron";
+import axios from 'axios';
+
+
 const app = express();
 
 //app.use(express.json());
+
+
 
 
 const allowedOrigins = ['https://networker-fe.vercel.app', 'http://networker-fe.vercel.app', 'http://localhost:3000'];
@@ -17,6 +23,32 @@ app.use(cors(corsOptions));
 app.use(express.json())
 
 app.use("/people", peopleRoutes)
+
+new CronJob('*/5 * * * *', function() {
+    axios.get('https://networker-be.onrender.com') // replace with your server url
+      .then(response => {
+        console.log('Server pinged successfully');
+      })
+      .catch(error => {
+        console.log('Error pinging server: ', error);
+      });
+  }, null, true, 'America/Chicago');
+
+
+
 app.listen(8000, () => {
     console.log("connected")
 })
+
+
+// use Cron, self pinging the server in every 10min
+//var CronJob = require('cron').CronJob;
+// var ping = require('ping');
+// var host = 'https://networker-be.onrender.com';
+
+// var job = new CronJob('* * * * * *', function() {
+//     ping.sys.probe(host, function(isAlive){
+//         var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+//         console.log(msg);
+//     });
+// }, null, true, 'America/Los_Angeles');
